a52c357a47b70b0dd6d2b2e51bda27a2f4e26d08
PatrykM <patrykxmajchrzak@gmail.com>
2020-10-19 21:01:43 +0200

    sum array and then check if even or odd

---
+++ sum_odd_even.js
@@ -0,0 +1,25 @@
+// Given a list of numbers, determine whether the sum of its elements is odd or even.
+// Give your answer as a string matching "odd" or "even".
+// If the input array is empty consider it as: [0] (array with a zero).
+// Example:
+// odd_or_even([0])          ==  "even"
+// odd_or_even([0, 1, 4])    ==  "odd"
+// odd_or_even([0, -1, -5])  ==  "even"
+
+arr = [1,2,3,4,5,1]
+
+
+function oddOrEven(array) {
+    if(array[0] == undefined){
+        return 'even'
+    }
+    const total = array.reduce((total, value) => {
+        return total + value
+    })
+    return (total % 2 === 1 ||total % 2 === -1) ? 'odd' : 'even'
+}
+
+//"best" answer
+// function oddOrEven(arr) {
+//     return arr.reduce((a,b)=>a+b,0) % 2 ? 'odd' : 'even';
+//   }
